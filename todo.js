578487dae18629304c1d119dcb6591be1ad5be24
Alec Klein <alekpklein@gmail.com>
2023-02-23 20:00:26 -0600

    populated todo list

---
+++ todo.js
@@ -0,0 +1,37 @@
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+let arrayOfTodos = [
+  {
+    userId: 14,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 20,
+    id: 2,
+    title: "delectus aut autem",
+    completed: false,
+  },
+];
+
+const fetchTodos = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => (arrayOfTodos = json));
+};
+
+const logTodos = () => {
+  console.log(arrayOfTodos);
+};
+
+const populateTodos = () => {
+  const todoList = document.querySelector('#todo-list');
+  for(let i = 0; i < arrayOfTodos.length; i++) {
+    const listItem = document.createElement('li');
+    let todo = arrayOfTodos[i]
+    const newText = document.createTextNode(todo.title);
+    listItem.appendChild(newText);
+    todoList.appendChild(listItem);
+  }
+};
